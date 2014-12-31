edb6a4027d8fc4113fcec7ee739d155c21f1a202
Ronald Mulero <Ron@Ronalds-MBP.lan>
2014-12-31 17:03:32 -0600

    Initial Commit

---
+++ MarcoPollo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarcoPollo
+//
+//  Created by Ronald Mulero on 12/31/14.
+//  Copyright (c) 2014 Ron Mulero, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarcoPollo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarcoPollo
+//
+//  Created by Ronald Mulero on 12/31/14.
+//  Copyright (c) 2014 Ron Mulero, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
